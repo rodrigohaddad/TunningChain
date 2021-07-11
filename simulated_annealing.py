2b4be6ca808e5a5bdcaeb95befc38517cbcedc10
rodrigohaddad <rgh@poli.ufrj.br>
2021-07-11 17:15:41 -0300

    simulated annealing, missing wnn and dataset

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,38 +1,71 @@
 import numpy as np
 from math import exp
+from random import choices
+from weightless_neural_network import WeighlessNetwok
+
 
 class SimulatedAnnealing():
-    def __init__(self, steps, temperature, t_function):
+    bounds = [(3, 10), (3, 36), (0.1, 0.9), (3, 10), (3, 10)]
+
+    def __init__(self, steps, temperature, t_function, data):
         self.temperature = temperature
         self.t_function = t_function
         self.steps = steps
         self.initial_state = list()
+        self.wn = WeighlessNetwok(data)
 
     def objective(self, candidates):
+        evals = list()
         for candidate in candidates:
-            pass
+            trained_wn = self.wn.train(candidate)
+            evals.append(self.wn.eval(trained_wn))
+        return evals
 
     def list_candidates(self, curr_state):
-        pass
+        candidates = list()
+        for arg in range(len(curr_state)):
+            new_state_ahead, new_state_behind = curr_state, curr_state
+            if curr_state[arg] == self.bounds[arg][0]:
+                new_state_behind[arg] = self.bounds[arg][1]
+            else:
+                new_state_ahead[arg] += 1
+
+            if curr_state[arg] == self.bounds[arg][1]:
+                new_state_ahead[arg] = self.bounds[arg][0]
+            else:
+                new_state_behind[arg] -= 1                    
+            candidates.append(new_state_ahead)
+            candidates.append(new_state_behind)
+            
+        return candidates
 
     def metropolis(self, zipped_diffs, curr_temp):
         m_list = list()
-        for candidate, diff in zipped_diffs:
+        for candidate, _, diff in zipped_diffs:
             if diff < 0:
-                 m_list.append((candidate, (1)*exp(-diff/curr_temp)))
+                 m_list.append((candidate, (1/10)*exp(-diff/curr_temp)))
         return m_list
-            
 
     def choose_next_state(self, zipped_candidates):
-        pass
+        filtered_candidates = list(filter(lambda x: x[1] >= 1, zipped_candidates))
+        population = list(range(0, len(filtered_candidates)))
+        weight = 1/len(filtered_candidates)
+        state_position = choices(population, [weight]*len(filtered_candidates))
 
-    def choose_next_state_metropolis(self):
-        pass
+        return filtered_candidates[state_position]
+
+    def choose_next_state_metropolis(self, zipped_candidates, curr_state, curr_state_eval):
+        population = list(range(0, len(zipped_candidates) + 1))
+        weights = list(list(zip(*zipped_candidates))[1])
+        weights.append(1 - sum(weights) if (1 - sum(weights) > 0) else 0)
+        state_position = choices(population, weights)
+        zipped_candidates.append((curr_state, curr_state_eval, 0))
+
+        return zipped_candidates[state_position]
 
     def simulate(self):
         best = self.initial_state
         best_eval = self.objective()
-        scores = list()
         curr_state, curr_state_eval = best, best_eval
         curr_temp = self.temperature
 
@@ -40,13 +73,14 @@ class SimulatedAnnealing():
             candidates = self.list_candidates(curr_state)
             candidates_eval = self.objective(candidates)
             diffs = [candidate_eval - curr_state_eval for candidate_eval in candidates_eval]
+            zipped_diffs = zip(candidates, candidates_eval, diffs)
             if np.where(diffs < 0 ):
-                zipped_diffs = zip(candidates, diffs)
                 metropolis = self.metropolis(zipped_diffs, curr_temp)
-                candidate, candidate_eval = self.choose_next_state_metropolis(metropolis)
+                candidate, candidate_eval, _ = self.choose_next_state_metropolis(metropolis, 
+                                                                                curr_state, 
+                                                                                curr_state_eval)
             else:
-                zipped_candidates = zip(candidates, candidates_eval)
-                candidate, candidate_eval = self.choose_next_state(zipped_candidates)
+                candidate, candidate_eval, _ = self.choose_next_state(zipped_diffs)
                 best, best_eval = candidate, candidate_eval
             curr_state, curr_state_eval = candidate, candidate_eval
             curr_temp = self.t_function(i, self.temperature)
--- weightless_neural_network.py
+++ weightless_neural_network.py
@@ -2,8 +2,24 @@ import wisardpkg as wp
 import numpy as np
 from thermometer import ThermometerEncoder
 
+clus_kwargs = {'resolution': list(range(3, 11)),
+                'addressSize': list(range(3, 36)),
+                'minScore': [x*0.1 for x in range(1, 9)],
+                'threshold': list(range(3, 11)),
+                'discriminatorLimit': list(range(3, 11))}
+
 class WeighlessNetwok():  
+    def __init__(self, data):
+        #apply all the thermoter combinations
+        pass
+
     def simple_thermometer(self, arr, minimum, maximum, resolution) -> list:
         therm = ThermometerEncoder(
             maximum=maximum, minimum=minimum, resolution=resolution)
         return [np.uint8(therm.encode(x)).flatten() for x in arr]
+
+    def train(self):
+        pass
+
+    def eval(self):
+        pass
